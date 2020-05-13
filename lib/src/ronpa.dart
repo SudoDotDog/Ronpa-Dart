99ce424616d43e611784d911c12ae5d8b5226b02
WMXPY <wm@mengw.io>
2020-05-13 15:44:55 -0500

    implment multiple story functions

--- lib/src/ronpa.dart
+++ lib/src/ronpa.dart
@@ -1,1 +1,43 @@
-class Ronpa {}
+import 'package:ronpa/src/story.dart';
+
+class Ronpa {
+  final Map<String, Story> storyMap = new Map();
+  final List<Story> storyList = [];
+
+  Ronpa();
+
+  List<Story> filterStories(
+    bool Function(Story, int, List<Story>) filter,
+  ) {
+    final List<Story> stories = [];
+    for (int i = 0; i < this.storyList.length; i++) {
+      final Story current = this.storyList[i];
+      if (filter(current, i, this.storyList)) {
+        stories.add(current);
+      }
+    }
+
+    return stories;
+  }
+
+  List<Map<String, dynamic>> flat() {
+    final List<Map<String, dynamic>> records = [];
+    for (final Story story in this.storyList) {
+      records.addAll(story.flat());
+    }
+
+    return records;
+  }
+
+  List<Map<String, dynamic>> flatSome(
+    bool Function(Story, int, List<Story>) filter,
+  ) {
+    final List<Story> stories = this.filterStories(filter);
+    final List<Map<String, dynamic>> records = [];
+    for (final Story story in stories) {
+      records.addAll(story.flat());
+    }
+
+    return records;
+  }
+}
--- lib/src/story.dart
+++ lib/src/story.dart
@@ -18,9 +18,56 @@ class Story {
 
   void addRecord(Map<String, dynamic> record) {}
 
+  void updateExtras(Map<String, dynamic> extras) {
+    final Thesis thesis = this.getThesis();
+    this.thesis = Thesis(
+      thesis.insiders,
+      extras: thesis.extras,
+    );
+  }
+
+  Thesis getThesis() {
+    if (this.thesis != null) {
+      return this.thesis;
+    }
+    throw ("Thesis does not exist");
+  }
+
+  List<Bullet> filterBullets(
+    bool Function(Bullet, int, List<Bullet>) filter,
+  ) {
+    final List<Bullet> bullets = [];
+    for (int i = 0; i < this.bulletList.length; i++) {
+      final Bullet current = this.bulletList[i];
+      if (filter(current, i, this.bulletList)) {
+        bullets.add(current);
+      }
+    }
+
+    return bullets;
+  }
+
   List<Map<String, dynamic>> flat() {
     final List<Map<String, dynamic>> bulletRecordList =
         this.bulletList.map((Bullet each) => each.record()).toList();
+    final Map<String, dynamic> thesisRecord = this.getThesisRecord();
+
+    if (thesisRecord != null) {
+      final List<Map<String, dynamic>> result = [thesisRecord];
+      result.addAll(bulletRecordList);
+
+      return result;
+    }
+
+    return bulletRecordList;
+  }
+
+  List<Map<String, dynamic>> flatSome(
+    bool Function(Bullet, int, List<Bullet>) filter,
+  ) {
+    final List<Bullet> bullets = this.filterBullets(filter);
+    final List<Map<String, dynamic>> bulletRecordList =
+        bullets.map((Bullet each) => each.record()).toList();
     final Map<String, dynamic> thesisRecord = this.getThesisRecord();
 
     if (thesisRecord != null) {
